ebf2eb7b190a80d80923cb915f29abf19f5b58f0
David Marques <dmsm2000@hotmail.com>
2023-06-17 17:10:12 +0100

    Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,7 +16,7 @@ const ServiceCard = ({ index, title, icon }) => {
           scale: 1,
           speed: 450,
         }}
-        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[180px] flex justify-evenly items-center flex-col">
         <img src={icon} alt={title} className="w-16 h-16 object-contain" />
         <h3 className="text-taupe text-[18px] font-bold text-center">
           {title}
